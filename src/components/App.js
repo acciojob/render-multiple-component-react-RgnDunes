239f577918f421d9873ee9cb92a98eb9448e4a3f
RgnDunes <divyansh.singh@razorpay.com>
2023-02-22 00:40:54 +0530

    Add solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
+import ProjectName from "./ProjectName";
+import ProjectDescription from "./ProjectDescription";
+
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test="project-name">Portfolio-v4</h1>
-      <h1 data-ns-test="project-description">Random</h1>
+      <ProjectName />
+      <ProjectDescription />
     </div>
   );
 };
---
+++ src/components/ProjectName.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectName = () => {
+  return <h1 data-ns-test="project-name">Portfolio-v4</h1>;
+};
+
+export default ProjectName;
