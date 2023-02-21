92111c6290627fd1a9c29b4bd7f79358b52b2e5e
RgnDunes <divyansh.singh@razorpay.com>
2023-02-22 00:26:07 +0530

    Add solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <div data-ns-test="project-name">Portfolio-v4</div>
+      <div data-ns-test="project-description">Random</div>
+    </div>
+  );
+};
 
 export default App;
